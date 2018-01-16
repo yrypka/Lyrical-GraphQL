dce762c8d663505d17f7228b1d13296c056b8734
Yuriy Rypka <yurarypka@gmail.com>
2018-01-17 00:15:10 +0200

    render lyric list

--- client/components/SongDetails.js
+++ client/components/SongDetails.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router';
 
 import fetchSong from '../queries/fetchSong';
 import LyricCreate from './LyricCreate';
+import LyricList from './LyricList';
 
 class SongDetails extends Component {
   render() {
@@ -15,6 +16,7 @@ class SongDetails extends Component {
       <div>
         <Link to="/">Back</Link>
         <h3>{song.title}</h3>
+        <LyricList lyrics={song.lyrics} />
         <LyricCreate songId={song.id} />
       </div>
     );
