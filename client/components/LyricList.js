721d0b4b9f6f3d5d06a6d287b217d23ab646306b
Yuriy Rypka <yurarypka@gmail.com>
2018-01-17 02:39:32 +0200

    implement like lyric feature

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
 class LyricList extends Component {
+  onLyricLike(id) {
+    this.props.mutate({ variables: { id } });
+  }
+
   renderLyrics() {
-    return this.props.lyrics.map(({ id, content }) => {
+    return this.props.lyrics.map(({ id, likes, content }) => {
       return (
         <li key={id} className="collection-item">
           {content}
+          <div className="vote-box">
+            <i className="material-icons" onClick={() => this.onLyricLike(id)}>thumb_up</i>
+            {likes}
+          </div>
         </li>
       );
     })
@@ -20,4 +30,13 @@ class LyricList extends Component {
   }
 }
 
-export default LyricList;
+const mutation = gql`
+  mutation LikeLyric($id: ID) {
+    likeLyric(id: $id) {
+      id
+      likes
+    }
+  }
+`;
+
+export default graphql(mutation)(LyricList);
